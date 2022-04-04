ee2dc7e6ceec24920e40d7747bfa5db4a722c659
Nurbek <68291178+Serzhanov@users.noreply.github.com>
2022-04-04 18:45:02 +0300

    done with deploying

--- CL&IHM/src/environments/environment.prod.ts
+++ CL&IHM/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDzodoYmYMYfZXinR_LDSBIxOtdg6-cFvk",
+    authDomain: "l3-todolist.firebaseapp.com",
+    projectId: "l3-todolist",
+    storageBucket: "l3-todolist.appspot.com",
+    messagingSenderId: "303318412574",
+    appId: "1:303318412574:web:72f08f3647580b83612d0f"
+  }
 };
